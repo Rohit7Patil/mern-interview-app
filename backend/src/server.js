112464fd877bea21c7bb351a0cc04b36b2a1feff
Rohit Patil <rohitpatil0522@gmail.com>
2026-01-24 20:40:30 +0530

    clerk, inngest & stream setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -10,20 +11,25 @@ app.get("/health", (_, res) => {
   res.status(200).json({ msg: "API is up & running..." });
 });
 
-app.get("/books", (_, res) => {
-  res.status(200).json({ msg: "API is up & running..." });
-});
-
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
+  app.get("/{*any}", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-app.listen(ENV.PORT, () =>
-  console.log(`Server is running on port ${ENV.PORT}`),
-);
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log(`Server is running on port ${ENV.PORT}`),
+    );
+  } catch (error) {
+    console.error("Error starting server\n", error);
+  }
+};
+
+startServer();
 
 export default app;
