e9d9663a529be2582c4db9ce9c39528a7abaacf2
Rohit Patil <rohitpatil0522@gmail.com>
2026-01-26 09:24:17 +0530

    stream-inngest config

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({
   id: "mern-interview-app",
@@ -22,6 +23,12 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   },
 );
 
@@ -33,6 +40,8 @@ const deleteUser = inngest.createFunction(
     const { id } = event.data;
 
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
   },
 );
 
